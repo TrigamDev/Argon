96e3d305c97a90e6bc3857a69cccfa743f2735aa
TrigamDev <trigambusiness@gmail.com>
2024-10-27 20:44:23 -0400

    Oopsies, forgot Bun's types

--- server/src/util/conversion.ts
+++ server/src/util/conversion.ts
@@ -1,5 +1,4 @@
 import { PassThrough, Readable } from "stream"
-import FfmpegCommand from 'fluent-ffmpeg'
 
 export function bufferToReadStream(buffer: Buffer): Readable {
 	let stream = new PassThrough()
